610e943af7b2e4694912297714dccb7f5e2e583f
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-11-12 13:27:32 -0300

    fix: add type hint

--- src/csc27_ML_distributed/server/models/config.py
+++ src/csc27_ML_distributed/server/models/config.py
@@ -2,7 +2,7 @@ import os
 import argparse
 from pydantic_settings import BaseSettings
 
-def _parse_args():
+def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Run the app with options.")
     parser.add_argument("--local", action="store_true", help="Run the app in local mode")
     return parser.parse_args()
