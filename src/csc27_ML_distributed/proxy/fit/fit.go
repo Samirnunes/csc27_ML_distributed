27b546d78bca123fb239095b24fd75748904f0d4
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-11-15 12:52:22 -0300

    feat: add goroutine to fit.go

--- src/csc27_ML_distributed/proxy/fit/fit.go
+++ src/csc27_ML_distributed/proxy/fit/fit.go
@@ -13,8 +13,6 @@ func Fit(){
 	for _, client := range servers.Clients {
 		defer client.Close()
 		var reply string
-		if err := client.Call("fit", nil, &reply); err != nil {
-			log.Fatalf("Error during fitting: %v", err)
-		}
+		go client.Call("fit", nil, &reply)
 	}
 } 
