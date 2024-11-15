f56b0fcea856455aea44a85c0c0550d6082ac86e
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-11-15 19:28:25 -0300

    feat: add Dockerfile.proxy and complete docker-compose.yml

--- src/csc27_ML_distributed/proxy/app/app.go
+++ src/csc27_ML_distributed/proxy/app/app.go
@@ -21,6 +21,17 @@ func FitHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Models trained successfully"))
 }
 
+// Evaluate handler
+func EvaluateHandler(w http.ResponseWriter, r *http.Request) {
+	metrics := evaluate.Evaluate()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(metrics); err != nil {
+		log.Fatalf("Error encoding response: %v", err)
+	}
+}
+
 // Predict handler
 func PredictHandler(w http.ResponseWriter, r *http.Request) {
 	var features map[string]interface{}
@@ -39,24 +50,13 @@ func PredictHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Evaluate handler
-func EvaluateHandler(w http.ResponseWriter, r *http.Request) {
-	metrics := evaluate.Evaluate()
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(metrics); err != nil {
-		log.Fatalf("Error encoding response: %v", err)
-	}
-}
-
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/v1/ml-distributed/fit", FitHandler).Methods("POST")
-	r.HandleFunc("/v1/ml-distributed/predict", PredictHandler).Methods("POST")
 	r.HandleFunc("/v1/ml-distributed/evaluate", EvaluateHandler).Methods("POST")
+	r.HandleFunc("/v1/ml-distributed/predict", PredictHandler).Methods("POST")
 
-	log.Println("Server started at localhost:8080")
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	log.Println("Server started at 0.0.0.0:80")
+	log.Fatal(http.ListenAndServe("0.0.0.0:80", r))
 }
